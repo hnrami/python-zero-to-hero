92a42636a044088d6278e74624e6cc8197b6082d
hnrami <ramihemang@gmail.com>
2025-04-01 20:06:38 +0530

    update code base

---
+++ src/module-one/par-two-basic-data-type.py
@@ -0,0 +1,36 @@
+"""
+
+Built-in Data Types
+
+Text Type:	str
+Numeric Types:	int, float, complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset
+Boolean Type:	bool
+Binary Types:	bytes, bytearray, memoryview
+None Type:	NoneType
+"""
+
+
+# You can get the data type of any object by using the type() function:
+
+typeVariable_str = "Type of variable"
+print(type(typeVariable_str))
+
+x = 20
+print(x)
+
+x = 20.5
+print(x)
+
+x = 3 + 7j
+print(x)
+
+x = ["apple", "banana", "cherry"]
+print(x)
+
+
+x = ("apple", "banana", "cherry")
+print(x)
+
